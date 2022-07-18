1838b769c413ce28040f835b4ed564833946d16b
Parker Hudson <gphudson97@hotmail.com>
2022-07-18 04:42:05 -0400

    Deleted unused files. Created patient object.

---
+++ COP3703_TeamProject_V1/src/project/models/patient.java
@@ -0,0 +1,7 @@
+package COP3703_TeamProject_V1.src.project.models;
+import java.util.*;
+
+public class patient {
+    private String apptNum, roomNum, patientSSN, doctorID, confirmedPSSN;
+    private 
+}
