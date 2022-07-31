d31f43eba005c23d1897badbdf975af4afc75715
GPhud <gphudson97@hotmail.com>
2022-07-31 02:39:57 -0400

    AppointmentEdit frame completed

--- COP3703_TeamProject_V1/src/project/views/Frame_AppointmentEdit.java
+++ COP3703_TeamProject_V1/src/project/views/Frame_AppointmentEdit.java
@@ -3,19 +3,29 @@ package project.views;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.SpinnerDateModel;
 import javax.swing.SwingConstants;
+import javax.swing.text.DateFormatter;
 
 import net.proteanit.sql.DbUtils;
+import project.common.input_verify;
+import project.common.jdbc_connection;
 
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
@@ -23,10 +33,17 @@ import javax.swing.JSpinner;
 import com.toedter.calendar.JDateChooser;
 
 public class Frame_AppointmentEdit extends JFrame {
-	private JTextField textField_4;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField searchField;
+	private JTextField docIDField;
+	private JTextField roomNumField;
+	private JTextField patientSSNField;
+	private JDateChooser dateChooser;
+	private JSpinner spinner;
+	private JButton btnBack;
+	private JButton btnSave;
+	private JButton btnLoad;
+	private JButton btnDelete;
+	Connection connection = null;
 
 	/**
 	 * Launch the application.
@@ -56,6 +73,8 @@ public class Frame_AppointmentEdit extends JFrame {
 	}
 	
 	private void init_components() {
+		connection = jdbc_connection.dbConnection(); //db connection
+	
 		setBounds(100, 100, 733, 466);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -74,18 +93,18 @@ public class Frame_AppointmentEdit extends JFrame {
 		lblEnterAppointment.setBounds(10, 71, 196, 26);
 		getContentPane().add(lblEnterAppointment);
 		
-		textField_4 = new JTextField();
-		textField_4.setFont(new Font("Dialog", Font.PLAIN, 16));
-		textField_4.setColumns(10);
-		textField_4.setBounds(10, 96, 185, 26);
-		getContentPane().add(textField_4);
+		searchField = new JTextField();
+		searchField.setFont(new Font("Dialog", Font.PLAIN, 16));
+		searchField.setColumns(10);
+		searchField.setBounds(10, 96, 185, 26);
+		getContentPane().add(searchField);
 		
-		JButton btnLoad = new JButton("Load");
+		btnLoad = new JButton("Load");
 		btnLoad.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnLoad.setBounds(203, 97, 94, 25);
 		getContentPane().add(btnLoad);
 		
-		JButton btnDelete = new JButton("Delete");
+		btnDelete = new JButton("Delete");
 		btnDelete.setFont(new Font("Dialog", Font.PLAIN, 16));
 		btnDelete.setBounds(307, 97, 94, 25);
 		getContentPane().add(btnDelete);
@@ -120,57 +139,52 @@ public class Frame_AppointmentEdit extends JFrame {
 		lblDoctorName.setBounds(97, 309, 129, 20);
 		getContentPane().add(lblDoctorName);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(236, 312, 130, 20);
-		getContentPane().add(textField);
+		docIDField = new JTextField();
+		docIDField.setColumns(10);
+		docIDField.setBounds(236, 312, 130, 20);
+		getContentPane().add(docIDField);
 		
-		JSpinner spinner = new JSpinner();
+		spinner = new JSpinner();
 		spinner.setBounds(236, 279, 130, 20);
 		getContentPane().add(spinner);
 		
-		JDateChooser dateChooser = new JDateChooser();
+		SpinnerDateModel spinnermodel = new SpinnerDateModel();
+		spinnermodel.setCalendarField(Calendar.MINUTE);
+		spinner.setModel(spinnermodel);
+		getContentPane().add(spinner);
+		
+		JSpinner.DateEditor editor = new JSpinner.DateEditor(spinner, "hh:mm a");
+		DateFormatter formatter = (DateFormatter)editor.getTextField().getFormatter();
+		formatter.setAllowsInvalid(false); 
+		formatter.setOverwriteMode(true);
+		spinner.setEditor(editor);
+		
+		dateChooser = new JDateChooser();
 		dateChooser.getCalendarButton().setFont(new Font("Dialog", Font.PLAIN, 16));
 		dateChooser.setFont(new Font("Dialog", Font.PLAIN, 16));
 		dateChooser.setDateFormatString("MM/dd/yyyy");
 		dateChooser.setBounds(236, 244, 130, 20);
 		getContentPane().add(dateChooser);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(236, 214, 130, 20);
-		getContentPane().add(textField_1);
+		roomNumField = new JTextField();
+		roomNumField.setColumns(10);
+		roomNumField.setBounds(236, 214, 130, 20);
+		getContentPane().add(roomNumField);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(236, 183, 130, 20);
-		getContentPane().add(textField_2);
+		patientSSNField = new JTextField();
+		patientSSNField.setColumns(10);
+		patientSSNField.setBounds(236, 183, 130, 20);
+		getContentPane().add(patientSSNField);
 		
-		JLabel lblNewLabel_1 = new JLabel("Procedures");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
-		lblNewLabel_1.setFont(new Font("Dialog", Font.BOLD, 16));
-		lblNewLabel_1.setBounds(462, 174, 97, 32);
-		getContentPane().add(lblNewLabel_1);
+		btnBack = new JButton("Back");
+		btnBack.setFont(new Font("Dialog", Font.BOLD, 18));
+		btnBack.setBounds(601, 336, 93, 66);
+		getContentPane().add(btnBack);
 		
-		JCheckBox chckbxCheckup = new JCheckBox("Checkup");
-		chckbxCheckup.setFont(new Font("Dialog", Font.PLAIN, 12));
-		chckbxCheckup.setBounds(462, 216, 97, 23);
-		getContentPane().add(chckbxCheckup);
-		
-		JCheckBox chckbxImmunizations = new JCheckBox("Immunizations");
-		chckbxImmunizations.setFont(new Font("Dialog", Font.PLAIN, 12));
-		chckbxImmunizations.setBounds(462, 242, 113, 23);
-		getContentPane().add(chckbxImmunizations);
-		
-		JCheckBox chckbxPrescription = new JCheckBox("Prescription");
-		chckbxPrescription.setFont(new Font("Dialog", Font.PLAIN, 12));
-		chckbxPrescription.setBounds(462, 267, 97, 23);
-		getContentPane().add(chckbxPrescription);
-		
-		JCheckBox chckbxConsultation = new JCheckBox("Consultation");
-		chckbxConsultation.setFont(new Font("Dialog", Font.PLAIN, 12));
-		chckbxConsultation.setBounds(462, 295, 97, 23);
-		getContentPane().add(chckbxConsultation);
+		btnSave = new JButton("Save");
+		btnSave.setFont(new Font("Dialog", Font.BOLD, 18));
+		btnSave.setBounds(601, 259, 93, 66);
+		getContentPane().add(btnSave);
 		
 		
 		
@@ -179,6 +193,105 @@ public class Frame_AppointmentEdit extends JFrame {
 	}
 	
 	private void create_events() {
+		// saves the user edited fields
+				btnSave.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						edit_info(searchField.getText());
+					}
+				}); 
+				
+				// loads all the fields for searched appt
+				btnLoad.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						display_info(searchField.getText());
+					}
+				});
+				
+				// deletes
+				btnDelete.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int action = JOptionPane.showConfirmDialog(null, "Do you really want to delete?","Choose an option",JOptionPane.YES_NO_OPTION);
+						if (action == 0) {
+							info_delete(searchField.getText());
+						}
+					}
+				});
+				
+				
+				// returns to appt screen
+				btnBack.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						Frame_Appointment frame_appt = new Frame_Appointment();
+						frame_appt.setVisible(true);
+						dispose();
+					}
+				});
+	}
+		
+		PreparedStatement pStmt;
+		
+		//displays all the information on searched appt
+		private void display_info(String id) {
+			try {
+				String query = "SELECT * FROM appointments WHERE APPTNUM = '" + id + "'";
+				pStmt = connection.prepareStatement(query);
+				ResultSet rs = pStmt.executeQuery();
+				while (rs.next()) {
+					patientSSNField.setText(rs.getString(5));
+					roomNumField.setText(rs.getString(2));
+					dateChooser.setDate(rs.getDate(3));
+					spinner.setValue(rs.getTimestamp(6));
+					docIDField.setText(rs.getString(4));
+				}	
+				pStmt.close();
+			} catch (SQLException e1) {	
+				e1.printStackTrace();
+				JOptionPane.showMessageDialog(null, "Uhhh something broke..");
+			}
+		}
+		
+		// edits doctor information
+		private void edit_info(String id) {
+			
+			
+				try {
+					
+					java.util.Date d = (java.util.Date) spinner.getValue();
+					java.sql.Timestamp sd = new java.sql.Timestamp(d.getTime());
+					
+					java.util.Date utilDate = (java.util.Date) dateChooser.getDate();
+					java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+					
+					String query = "UPDATE appointments SET patients_ssn=?, roomnum=?, appt_date=?, appt_time=?, doctors_doctorid=? WHERE apptnum=?";
+					pStmt = connection.prepareStatement(query);
+					pStmt.setString(1, patientSSNField.getText());	
+					pStmt.setString(2, roomNumField.getText());	
+					pStmt.setDate(3, sqlDate);
+					pStmt.setTimestamp(4, sd);
+					pStmt.setString(5, docIDField.getText());
+					pStmt.setString(6, id);
+					pStmt.execute();
+					JOptionPane.showMessageDialog(null, "Appointment Updated");	
+					pStmt.close();
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+					
+				}
+			
 		
 	}
+		
+		private void info_delete(String num) {
+			String query = "DELETE FROM appointments WHERE apptnum=?";
+			try {
+				PreparedStatement pStmt = connection.prepareStatement(query);
+				pStmt.setString(1, num);	
+				pStmt.executeQuery();
+				JOptionPane.showMessageDialog(null, "Appointment Deleted");
+				pStmt.close();
+			} catch (java.sql.SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
 }
